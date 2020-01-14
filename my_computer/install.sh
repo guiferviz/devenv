c0c062c3bc6582ec0059a412c487c768882fefe6
guiferviz <guiferviz@gmail.com>
2020-01-14 13:56:09 +0100

    Installing nodejs and yarn.

--- my_computer/install.sh
+++ my_computer/install.sh
@@ -66,3 +66,11 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 1
 # Install Python venv.
 sudo apt-get install python3-venv
 
+# Install nodejs.
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Yarn.
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn
+
