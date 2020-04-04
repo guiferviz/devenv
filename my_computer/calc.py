a7a53f22ad00f57719c56dacfacccf4228d8bc93
guiferviz <guiferviz@gmail.com>
2020-04-04 13:38:03 +0200

    Fixing but, using 'pass;' instead of ';' to avoid errors. ';' is not a valid statement in Python.

--- my_computer/calc.py
+++ my_computer/calc.py
@@ -4,12 +4,26 @@ import sys
 import math
 
 
+def factors(n): 
+    factors = []
+    while n % 2 == 0: 
+        factors.append(2) 
+        n = n / 2
+    for i in range(3, int(math.sqrt(n)) + 1, 2): 
+        while n % i== 0: 
+            factors.append(i) 
+            n = n / i 
+    if n > 2: 
+        factors.append(n)
+    return factors
+
+
 def combination(n, k):
     return math.factorial(n) / (math.factorial(k) * math.factorial(n - k))
 
 
 def main():
-    txt = ";" + " ".join(sys.argv[1:])
+    txt = "pass;" + " ".join(sys.argv[1:])
     code, expression = txt.rsplit(";", 1)
     exec(code)
     res = eval(expression)
